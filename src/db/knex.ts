aaa151092b4259a9bc08cf3bb9d7b45b7b0009cf
maora <marinaoliveira.r07@gmail.com>
2023-01-28 21:02:48 -0300

    fix: connection string

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -2,11 +2,13 @@ import knexLib from 'knex';
 
 export const knex = knexLib({
   client: 'pg',
-  connection: {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'aroacedb',
-  },
+  connection: process.env.DATABASE_URL,
+  // connection: {
+  //   connectString:process.env.DATABASE_URL,
+  //   // host: 'localhost',
+  //   // port: 5432,
+  //   // user: 'postgres',
+  //   // password: 'postgres',
+  //   // database: 'aroacedb',
+  // },
 });
